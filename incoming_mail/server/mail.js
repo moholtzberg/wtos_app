ed012db1a99f51ae22c42c68c3af6cf38f8b474b
Moshe Holtzberg <moholtzberg@gmail.com>
2014-03-18 17:31:07 -0400

    changed again

--- incoming_mail/server/mail.js
+++ incoming_mail/server/mail.js
@@ -1,8 +1,8 @@
 Meteor.Router.add('/messages/incoming', 'POST', function() {
 	console.log("======================ROUTER - REC");
-	var params = this.request.body;
+	var params = this.request.body.toString();
 	// var a = JSON.stringify(params);
-	var b = JSON.parse(params.toString())
+	var b = JSON.parse(params)
 	console.log(b)
 	// Messages.update({_id: params.message_id}, {$set: {delivered: {status: true, time_stamp: new Date()}}});
 	console.log("============================");
